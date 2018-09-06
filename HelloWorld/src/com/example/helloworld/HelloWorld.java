64d909bba581584bb2e746eab85319eb07fc6d0c
amirulmaricar <Amirul@comp.nus.edu.sg>
2018-09-06 18:10:24 +0800

    Added Hello World File

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by Amirul Maricar on 19/08/2018
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("it's me, Wombat!");
+    }
+}
